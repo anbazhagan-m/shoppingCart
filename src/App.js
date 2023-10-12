e8b0cd08ca5020623216e464c3916fadd6c595c2
Administrator <Administrator@PTRAINING9.LOCAL>
2023-10-12 16:22:43 +0530

    initialcommit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './style/app.css';
+import Card from './components/Card';
+function App() {
+  
+  return (
+    
+    <div className="App">
+      <header>
+        <div className="grid1200">
+        </div>
+      </header>
+      <main>
+        <div className="grid1200">
+          <Card />
+          <Card />
+          <Card />
+        </div>
+      </main> 
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Card.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div>
+      <div className="card">
+            <h1> Card Title </h1>
+      <p> This is a description </p>
+    </div>
+    </div>
+  )
+}
+
+export default Card
